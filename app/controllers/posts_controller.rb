60641075b52b8076f1986b3bce48cc510e9e65a4
zhaoyue <dourayme321@163.com>
2017-03-14 10:04:00 +0800

    implement my posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,6 +19,36 @@ class PostsController < ApplicationController
      end
    end
 
+   def edit
+     @group = Group.find(params[:group_id])
+     @post = Post.find(params[:id])
+   end
+
+   def update
+     @group = Group.find(params[:group_id])
+     @post = Post.find(params[:id])
+     if @post.update(post_params)
+       flash[:notice] = "success updated"
+       redirect_to account_posts_path
+     else
+       render :edit
+     end
+   end
+
+   def destroy
+     @group = Group.find(params[:group_id])
+     @post = Post.find(params[:id])
+     @post.destroy
+     redirect_to account_posts_path
+     flash[:alert] = "Post deleted"
+   end
+
+   private
+
+   def post_params
+     params.require(:post).permit(:content)
+   end
+
    private
 
    def post_params
